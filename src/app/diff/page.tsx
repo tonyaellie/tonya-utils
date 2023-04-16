70c71ec770ce8ab64dbbb7c6bb6fad6b01bbc507
tonya <tonya@tokia.dev>
2023-04-16 23:15:16 +0000

    feat: port diff

---
+++ src/app/diff/page.tsx
@@ -0,0 +1,16 @@
+import Diff from './Diff';
+
+// export const metadata = {
+//   title: 'Counter',
+//   description: 'Count stuff in text.',
+// };
+
+const DiffPage = () => {
+  return (
+    <>
+      <Diff />
+    </>
+  );
+};
+
+export default DiffPage;
