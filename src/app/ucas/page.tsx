fdfb11cf8dcd084d7425dc5c99a57a6417f7dfba
tonya <tonya@tokia.dev>
2023-04-17 11:06:33 +0000

    feat: switch ucas page

---
+++ src/app/ucas/page.tsx
@@ -0,0 +1,16 @@
+import Ucas from './Ucas';
+
+// export const metadata = {
+//   title: 'Counter',
+//   description: 'Count stuff in text.',
+// };
+
+const UcasPage = () => {
+  return (
+    <>
+      <Ucas />
+    </>
+  );
+};
+
+export default UcasPage;
