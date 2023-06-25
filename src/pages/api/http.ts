b04e2f84ac52324c76ff2e9317606d3d9afb2b4c
tonya <tonya@tokia.dev>
2023-06-25 22:19:41 +0000

    fix: just use zod

--- src/pages/api/http.ts
+++ src/pages/api/http.ts
@@ -1,12 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { z } from 'zod';
 
 const http = (req: NextApiRequest, res: NextApiResponse) => {
   // get param code
   const { code } = req.query;
-  if (typeof code !== 'number') {
+  if (
+    z
+      .string()
+      .regex(/^[0-9]+$/)
+      .length(3)
+      .parse(code as string)
+  ) {
     return res.status(200).json({ code: 200 });
   }
-  res.status(code).json({ code });
+  res.status(Number(code)).json({ code: Number(code) });
 };
 
 export default http;
