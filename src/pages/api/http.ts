4999a000d81ecf45844b71fd43156bde91fd2830
tonya <tonya@tokia.dev>
2023-06-25 22:23:21 +0000

    fix: i dumb

--- src/pages/api/http.ts
+++ src/pages/api/http.ts
@@ -4,13 +4,12 @@ import { z } from 'zod';
 const http = (req: NextApiRequest, res: NextApiResponse) => {
   // get param code
   const { code } = req.query;
-  if (
-    z
-      .string()
-      .regex(/^[0-9]+$/)
-      .length(3)
-      .parse(code as string)
-  ) {
+  const schema = z
+    .string()
+    .regex(/^[0-9]+$/)
+    .length(3)
+    .safeParse(code as string);
+  if (!schema.success) {
     return res.status(200).json({ code: 200 });
   }
   res.status(Number(code)).json({ code: Number(code) });
