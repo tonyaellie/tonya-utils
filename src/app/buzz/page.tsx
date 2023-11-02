63f7b70f14bcccbbd6b903ab88a5c427eceb933f
tonya <tonya@tokia.dev>
2023-11-02 09:28:34 +0000

    fix: disable buzz

--- src/app/buzz/page.tsx
+++ src/app/buzz/page.tsx
@@ -2,19 +2,18 @@ import type { Metadata } from 'next';
 
 import Buzz from './Buzz';
 
-import { readFile } from 'fs/promises';
-
 export const metadata: Metadata = {
   title: 'Buzz',
   description: 'Crack the spelling bee.',
 };
 
+// TODO: Fix importing words.txt
+
 const EncoderPage = async () => {
-  const words = (
-    await readFile('/home/tonya/tonya-utils/src/app/buzz/words.txt')
-  )
-    .toString()
-    .split('\n');
+  // const words = (await readFile('~/src/app/buzz/words.txt'))
+  // .toString()
+  // .split('\n');
+  const words = ['hello', 'world'];
 
   return <Buzz words={words} />;
 };
