5dd07727c5c995c03b0f369a5316b1b40ae7bec2
tonyaellie <tonya@tokia.dev>
2024-06-19 14:59:35 +0000

    feat: improve analyser

--- src/app/analyser/Analyser.tsx
+++ src/app/analyser/Analyser.tsx
@@ -6,19 +6,54 @@ import Sentiment from 'sentiment';
 import { syllable } from 'syllable';
 // @ts-expect-error text-readability is untyped
 import rs from 'text-readability';
+import { create } from 'zustand';
 
 import { AutosizeTextarea } from '@/components/ui/autosize-textarea';
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
+
+// TODO: make more efficient e.g. offload calculations to worker, debounce updates
 
 const sentiment = new Sentiment();
+
+const store = create<{
+  lineToSentiment: Map<string, number>;
+  getSentiment: (line: string) => number | undefined;
+  setSentiment: (line: string, sentiment: number) => void;
+}>((set, get) => ({
+  lineToSentiment: new Map(),
+  getSentiment: (line) => {
+    if (get().lineToSentiment.has(line)) {
+      return get().lineToSentiment.get(line)!;
+    }
+    return undefined;
+  },
+  setSentiment: (line, sentiment) => {
+    set(({ lineToSentiment }) => ({
+      lineToSentiment: new Map([...lineToSentiment]).set(line, sentiment),
+    }));
+  },
+}));
+
+const SyllableDisplay = ({ line }: { line: string }) => {
+  const syllableCount = useMemo(() => {
+    const syllableCount = store.getState().lineToSentiment.get(line);
+    if (syllableCount) {
+      return syllableCount.toString().padStart(3, '0');
+    }
+    const newSyllableCount = syllable(line);
+    store.getState().setSentiment(line, newSyllableCount);
+    return newSyllableCount.toString().padStart(3, '0');
+  }, [line]);
+
+  return (
+    <div className="flex w-[calc(100%-2px)] break-words text-sm">
+      <div>{syllableCount}</div>
+      <div className="min-w-0 whitespace-pre-wrap text-wrap text-transparent">
+        {line}
+      </div>
+    </div>
+  );
+};
 
 const Counter = () => {
   const [text, setText] = useState('');
@@ -26,8 +61,6 @@ const Counter = () => {
     const lines = text.split('\n');
     const chars = text.replace(/\n/g, '').split('');
     const readability = rs.fleschReadingEase(text);
-
-    console.log(sentiment.analyze(text));
 
     return {
       chars: chars.length,
@@ -83,32 +116,34 @@ const Counter = () => {
         </TableBody>
       </Table>
 
-      <div className="flex gap-4">
-        <div className="mt-[9px] flex flex-col">
+      <div className="relative flex">
+        <div className="absolute mt-[9px] w-[calc(100%-40px)]">
           {text.split('\n').map((line, index) => (
-            <div key={index} className="text-sm">
-              {syllable(line).toString().padStart(2, '0')}
-            </div>
+            <SyllableDisplay line={line} key={index} />
           ))}
         </div>
-        <div className="relative w-full">
-          <div className="absolute left-[13px] top-[9px] whitespace-pre-wrap break-words text-sm text-transparent">
-            {text.split(/([^a-zA-Z])/).map((word, i) => (
-              <span
-                key={`${i}-${word}`}
-                className={
-                  counts.sentiment.positive.includes(word.toLocaleLowerCase())
-                    ? 'rounded-sm bg-green-900'
-                    : counts.sentiment.negative.includes(
-                        word.toLocaleLowerCase()
-                      )
-                    ? 'rounded-sm bg-red-900'
-                    : ''
-                }
-              >
-                {word}
-              </span>
-            ))}
+        <div className="absolute left-[40px] w-[calc(100%-40px)]">
+          <div className="absolute left-[13px] top-[9px] mr-[12px] whitespace-pre-wrap break-words bg-background text-sm text-transparent">
+            {text.split(/([^a-zA-Z])/).map((word, i) =>
+              counts.sentiment.words.includes(word.toLocaleLowerCase()) ? (
+                <span
+                  key={`${i}-${word}`}
+                  className={
+                    counts.sentiment.positive.includes(word.toLocaleLowerCase())
+                      ? 'rounded-sm bg-green-900'
+                      : counts.sentiment.negative.includes(
+                          word.toLocaleLowerCase()
+                        )
+                      ? 'rounded-sm bg-red-900'
+                      : ''
+                  }
+                >
+                  {word}
+                </span>
+              ) : (
+                word
+              )
+            )}
           </div>
           <AutosizeTextarea
             className="absolute left-0 top-0 bg-transparent"
